ee4bb48c3c2c26121b61a75be4735c4b56422c55
GuFFy12 <jokers_hello_0d@icloud.com>
2024-09-04 16:53:44 +0500

    Игнорим логи от zapret

--- opt/etc/ndm/netfilter.d/zapret.sh
+++ opt/etc/ndm/netfilter.d/zapret.sh
@@ -3,6 +3,9 @@
 [ "$type" = "iptables" ] || exit 0
 [ "$table" = "mangle" ] || exit 0
 
-[ -n "$(pgrep -f /opt/zapret/)" ] || exit 0
+ZAPRET_BASE=${ZAPRET_BASE:-/opt/zapret}
+SCRIPT=${SCRIPT:-$ZAPRET_BASE/init.d/sysv/zapret}
 
-/opt/zapret/init.d/sysv/zapret start-fw
+[ -n "$(pgrep -f "$ZAPRET_BASE")" ] || exit 0
+
+$SCRIPT start-fw > /dev/null
