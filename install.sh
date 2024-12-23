c8d7c090000909fe5f4439470a33c593c0839150
GuFFy12 <jokers_hello_0d@icloud.com>
2024-12-23 20:33:17 +0500

    why

--- install.sh
+++ install.sh
@@ -14,16 +14,16 @@ replace_config_value() {
     sed -i "s|^$2=.*|$2=$3|" "$1"
 }
 
-rm_folder() {
+rm_dir() {
     [ -d "$1" ] && rm -r "$1"
 }
 
-if ! command -v ndmc; then
+if ! command -v ndmc >/dev/null; then
     echo "ndmc not found" >&2
     exit 1
 fi
 
-if ! command -v opkg; then
+if ! command -v opkg >/dev/null; then
     echo "opkg not found" >&2
     exit 1
 fi
@@ -43,13 +43,13 @@ if [ "${NDM_VERSION%%.*}" -lt 4 ]; then
 fi
 
 [ -f "$ZAPRET_SCRIPT" ] && "$ZAPRET_SCRIPT" stop
-rm_folder "$ZAPRET_BASE"
+rm_dir "$ZAPRET_BASE"
 curl -L "https://github.com/bol-van/zapret/releases/download/$ZAPRET_VERSION/zapret-$ZAPRET_VERSION.tar.gz" | tar -xz -C /opt/
 mv "/opt/zapret-$ZAPRET_VERSION/" "$ZAPRET_BASE"
 
 [ -f "$DNSMASQ_ROUTING_SCRIPT" ] && "$DNSMASQ_ROUTING_SCRIPT" stop
-rm_folder "$DNSMASQ_ROUTING_BASE"
-rm_folder "$TMP_DIR"
+rm_dir "$DNSMASQ_ROUTING_BASE"
+rm_dir "$TMP_DIR"
 git clone --depth=1 https://github.com/GuFFy12/keenetic-bypass.git "$TMP_DIR"
 find "$TMP_DIR/opt/" -type f | while read -r file; do
     dest="/opt/${file#"$TMP_DIR/opt/"}"
