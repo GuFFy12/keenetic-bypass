e4bfa836d3a92024551f8a02452c20ddd4b9f0d0
GuFFy12 <jokers_hello_0d@icloud.com>
2025-03-03 16:31:32 +0500

    разделение репо

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Keenetic Bypass
-
-![logo](https://github.com/user-attachments/assets/66087228-edfa-4957-82db-5bd7233f8ab9)
+# Keenetic Zapret
 
 ## Замечания
 
@@ -14,9 +12,6 @@
   Как пример таким устройством может быть оптический терминал который преобразует сигнал из оптоволокна.
   В этом случае данное устройство нужно перевести в режим моста (bridge).
 
-- Статическая маршрутизация на основе dnsmasq требует времени для сбора IP-адресов доменов.
-  В начале возможна нестабильность — просто обновите страницу пару раз что бы собрались возможные IP-адреса домена.
-
 ## Требования
 
 - [Keenetic OS](https://help.keenetic.com/hc/ru/articles/115000990005) версии 4.0 или выше.
@@ -29,30 +24,17 @@
 - В [компонентах KeeneticOS](https://help.keenetic.com/hc/ru/articles/360000358039) нужно включить `Kernel modules for Netfilter` или `Модули ядра подсистемы Netfilter`.
 - Купите, настройте и включите туннель [VPN](https://help.keenetic.com/hc/ru/articles/115005342025)
   или [Proxy](https://help.keenetic.com/hc/ru/articles/7474374790300).
-- Настройте пользовательский [DNS-over-HTTPS](https://help.keenetic.com/hc/ru/articles/360007687159) (пример: `https://dns.google/dns-query`).
-- Отключите [DNS от провайдера](https://help.keenetic.com/hc/ru/articles/360008609399).
-- Создайте записи DNS на адресе `192.168.1.1:5300` для доменов, к которым нужен доступ через туннель.
-- Пример списка доменов (не ставьте dns записи для GitHub до установки):
-
-  ```plaintext
-  chatgpt.com
-  openai.com
-  oaiusercontent.com
-  github.com
-  githubusercontent.com
-  githubcopilot.com
-  ```
 
 ### 2. Установка необходимых компонентов
 
 - Выполните следующую команду:
 
   ```sh
-  opkg update && opkg install curl && sh -c "$(curl -H 'Cache-Control: no-cache' -f -L https://raw.githubusercontent.com/GuFFy12/keenetic-bypass/refs/heads/main/install.sh)"
+  opkg update && opkg install curl && sh -c "$(curl -H 'Cache-Control: no-cache' -f -L https://raw.githubusercontent.com/GuFFy12/keenetic-zapret/refs/heads/main/install.sh)"
   ```
 
 - Или если хотите установить в режиме оффлайн, то разархивируйте на роутере
-  [файл релиз](https://github.com/GuFFy12/keenetic-bypass/releases/latest) и запустите:
+  [файл релиз](https://github.com/GuFFy12/keenetic-zapret/releases/latest) и запустите:
 
   ```sh
   sh install.sh
@@ -72,36 +54,3 @@
   ```sh
   /opt/zapret/ipset/get_config.sh
   ```
-
-### 4. Конфигурация Dnsmasq ([`/opt/dnsmasq_routing/dnsmasq.conf`](https://thekelleys.org.uk/dnsmasq/docs/dnsmasq-man.html))
-
-- Переменная `server` установлена автоматически на первый найденный `127.0.0.1:port`, который используется для получения DNS записей.
-  Чтобы вручную получить список DNS серверов, выполните команду:
-
-  ```sh
-  cat /tmp/ndnproxymain.stat
-  ```
-
-### 5. Конфигурация Dnsmasq Routing (`/opt/dnsmasq_routing/dnsmasq_routing.conf`)
-
-- Устройство отправляет DNS-запрос на маршрутизатор, который с помощью dnsmasq возвращает IP-адрес и добавляет его в ipset.
-  Все IP-адреса из ipset перенаправляются через туннель. Для работы системы важно, чтобы все DNS-запросы шли через маршрутизатор.
-- Переменные `INTERFACE` и `INTERFACE_SUBNET` установлены в зависимости от вашего выбора во время установки.
-  Чтобы вручную получить список интерфейсов выполните команду:
-
-  ```sh
-  ip -o -4 addr show
-  ```
-
-- Опционально настройте следующие переменные:
-  - `KILL_SWITCH` - если установлено в `1`, при отключении VPN или прокси трафик не будет направляться в сеть.
-  - `IPSET_TABLE_SAVE` - если установлено в `1`, таблица с IP-адресами будет сохранена при перезагрузке.
-  - `IPSET_TABLE` - имя таблицы ipset.
-  - `IPSET_TABLE_TIMEOUT` - тайм-аут для записей в таблице (`0` для неограниченного времени).
-  - `INTERFACE` - интерфейс выходного узла туннеля.
-  - `INTERFACE_SUBNET` - подсеть интерфейса выходного узла туннеля.
-  - `MARK` - маркер, используемый в iptables.
-
-## Настройка WireGuard на роутере в качестве сервера для улучшения интернета
-
-- Подробности: [WireGuard VPN](https://help.keenetic.com/hc/ru/articles/360010592379).
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-RELEASE_TAG=v1.1.8
+RELEASE_TAG=v1.0.0
 
 ZAPRET_BASE="${ZAPRET_BASE:-/opt/zapret}"
 ZAPRET_SCRIPT="${ZAPRET_SCRIPT:-"$ZAPRET_BASE/init.d/sysv/zapret_keenetic.sh"}"
@@ -10,10 +10,34 @@ ZAPRET_CONFIG="${ZAPRET_CONFIG:-"$ZAPRET_BASE/config"}"
 ZAPRET_INSTALL_BIN="${ZAPRET_INSTALL_BIN:-"$ZAPRET_BASE/install_bin.sh"}"
 ZAPRET_GET_CONFIG="${ZAPRET_GET_CONFIG:-"$ZAPRET_BASE/ipset/get_config.sh"}"
 
-DNSMASQ_ROUTING_BASE="${DNSMASQ_ROUTING_BASE:-/opt/dnsmasq_routing}"
-DNSMASQ_ROUTING_SCRIPT="${DNSMASQ_ROUTING_SCRIPT:-"$DNSMASQ_ROUTING_BASE/dnsmasq_routing.sh"}"
-DNSMASQ_ROUTING_CONFIG="${DNSMASQ_ROUTING_CONFIG:-"$DNSMASQ_ROUTING_BASE/dnsmasq_routing.conf"}"
-DNSMASQ_CONFIG="${DNSMASQ_CONFIG:-/opt/etc/dnsmasq.conf}"
+ask_yes_no() {
+	while true; do
+		echo "$1 (Y/N): "
+		read -r answer
+
+		case "$answer" in
+		[yY1]) return 0 ;;
+		[nN0]) return 1 ;;
+		*) echo Invalid choice ;;
+		esac
+	done
+}
+
+set_config_value() {
+	sed -i "s|^$2=.*|$2=$3|" "$1"
+}
+
+add_cron_job() {
+	if ! crontab -l 2>/dev/null | grep -Fq "$1"; then
+		(
+			crontab -l 2>/dev/null
+			echo "$1"
+		) | crontab -
+		echo "Cronjob added: $1"
+	else
+		echo "Cronjob already exists: $1"
+	fi
+}
 
 rm_dir() {
 	if [ -d "$1" ]; then
@@ -36,59 +60,6 @@ get_zapret_config_iface_wan() {
 	fi
 }
 
-get_dnsmasq_config_server() {
-	dnsmasq_config_server_port="$(awk '$1 == "127.0.0.1" {print $2; exit}' /tmp/ndnproxymain.stat)"
-
-	if [ -z "$dnsmasq_config_server_port" ]; then
-		echo No DNS server found >&2
-		return 1
-	fi
-
-	DNSMASQ_CONFIG_SERVER="${DNSMASQ_CONFIG_SERVER:-"127.0.0.1#$dnsmasq_config_server_port"}"
-}
-
-select_dnsmasq_routing_interface() {
-	interfaces=$(ip -o -4 addr show | awk '{print $1 " " $2 " " $4}')
-
-	if [ -z "$interfaces" ]; then
-		echo No interfaces found >&2
-		return 1
-	fi
-
-	echo Interface list:
-	echo "$interfaces" | awk '{print $1 " " $2 " (" $3 ")"}'
-
-	echo Enter interface tunnel number for dnsmasq routing:
-	read -r choice
-
-	selected_line="$(echo "$interfaces" | awk -F': ' -v choice="$choice" '$1 == choice')"
-	DNSMASQ_ROUTING_CONFIG_INTERFACE=$(echo "$selected_line" | awk '{print $2}')
-	DNSMASQ_ROUTING_CONFIG_INTERFACE_SUBNET=$(echo "$selected_line" | awk '{print $3}')
-
-	if [ -z "$DNSMASQ_ROUTING_CONFIG_INTERFACE" ] || [ -z "$DNSMASQ_ROUTING_CONFIG_INTERFACE_SUBNET" ]; then
-		echo Invalid interface choice >&2
-		return 1
-	fi
-
-	return 0
-}
-
-replace_config_value() {
-	sed -i "s|^$2=.*|$2=$3|" "$1"
-}
-
-add_cron_job() {
-	if ! crontab -l 2>/dev/null | grep -Fq "$1"; then
-		(
-			crontab -l 2>/dev/null
-			echo "$1"
-		) | crontab -
-		echo "Cronjob added: $1"
-	else
-		echo "Cronjob already exists: $1"
-	fi
-}
-
 if ! command -v ndmc >/dev/null; then
 	echo Command 'ndmc' not found >&2
 	exit 1
@@ -105,53 +76,40 @@ elif [ "${NDM_VERSION%%.*}" -lt 4 ]; then
 fi
 
 echo Installing packages...
-opkg update && opkg install coreutils-sort cron curl dnsmasq git-http grep gzip ipset iptables kmod_ndms xtables-addons_legacy
+opkg update && opkg install coreutils-sort cron curl grep gzip ipset iptables kmod_ndms xtables-addons_legacy
 
-echo Delete zapret...
+echo Installing Keenetic Zapret...
 delete_service "$ZAPRET_BASE" "$ZAPRET_SCRIPT"
-
-echo Installing Keenetic Bypass...
-delete_service "$DNSMASQ_ROUTING_BASE" "$DNSMASQ_ROUTING_SCRIPT"
 if [ -n "$(readlink -f "$0")" ]; then
 	cp -r opt/* /opt/
 else
 	TMP_DIR=$(mktemp -d)
-	RELEASE_FILE="keenetic-bypass-$RELEASE_TAG.tar.gz"
+	RELEASE_FILE="keenetic-zapret-$RELEASE_TAG.tar.gz"
 
-	curl -f -L -o "$TMP_DIR/$RELEASE_FILE" "https://github.com/GuFFy12/keenetic-bypass/releases/download/$RELEASE_TAG/$RELEASE_FILE"
+	curl -f -L -o "$TMP_DIR/$RELEASE_FILE" "https://github.com/GuFFy12/keenetic-zapret/releases/download/$RELEASE_TAG/$RELEASE_FILE"
 	tar -xvzf "$TMP_DIR/$RELEASE_FILE" -C "$TMP_DIR" >/dev/null
 	cp -r "$TMP_DIR/opt/"* /opt/
 	rm -rf "$TMP_DIR"
 fi
 
-echo Configuring zapret...
-"$ZAPRET_INSTALL_BIN"
-"$ZAPRET_GET_CONFIG"
-
 echo Changing the settings...
 if ! get_zapret_config_iface_wan; then
 	echo Failed to retrieve WAN interface for zapret >&2
 	exit 1
-elif ! get_dnsmasq_config_server; then
-	echo Failed to retrieve DNS server for dnsmasq >&2
-	exit 1
-elif ! select_dnsmasq_routing_interface; then
-	echo Failed to retrieve routing interface for dnsmasq routing >&2
-	exit 1
 fi
 
 replace_config_value "$ZAPRET_CONFIG" "IFACE_WAN" "$ZAPRET_CONFIG_IFACE_WAN"
-replace_config_value "$DNSMASQ_CONFIG" "server" "$DNSMASQ_CONFIG_SERVER"
-replace_config_value "$DNSMASQ_ROUTING_CONFIG" "INTERFACE" "$DNSMASQ_ROUTING_CONFIG_INTERFACE"
-replace_config_value "$DNSMASQ_ROUTING_CONFIG" "INTERFACE_SUBNET" "$DNSMASQ_ROUTING_CONFIG_INTERFACE_SUBNET"
 
-echo Configuring cron jobs...
-add_cron_job "0 0 * * * $DNSMASQ_ROUTING_SCRIPT save"
-add_cron_job "0 0 * * * $ZAPRET_GET_CONFIG"
+echo Configuring zapret...
+"$ZAPRET_INSTALL_BIN"
+
+if ask_yes_no "Create cron job to auto update zapret ipset list?"; then
+	add_cron_job "0 0 * * * $ZAPRET_GET_CONFIG"
+fi
 
 echo Running zapret...
 "$ZAPRET_SCRIPT" start
-echo Running dnsmasq routing...
-"$DNSMASQ_ROUTING_SCRIPT" start
+echo Downloading latest ipset domains list...
+"$ZAPRET_GET_CONFIG"
 
-echo Components have been successfully installed. For further configuration please refer to README.md file!
+echo Zapret have been successfully installed. For further configuration please refer to README.md file!
